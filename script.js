19b9604e58d48592a4ed088d39c914aa51415c89
laila <91194082+Lailaismyname@users.github.com>
2023-01-13 19:44:50 +0100

    nav

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const offCanvasNav = document.getElementById('offCanvasNav');
 const moreBtn = document.querySelector('.more-menu');
+const mainContent = document.querySelector('main');
 
 moreBtn.addEventListener('click', (e)=>{
     offCanvasNav.style.display = 'block';
+    mainContent.style.display = 'none';
 })
 offCanvasNav.addEventListener('click', (e)=>{
     if(e.target.id == "offCanvasNav"){
         offCanvasNav.style.display = 'none';
+        mainContent.style.display = 'flex';
     }
 })
